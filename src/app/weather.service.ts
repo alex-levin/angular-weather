457173f1d2fc58ceb3112d9db47d586446926bfe
Alex Levin <alex@email.com>
2018-03-13 19:47:10 -0400

    added HttpClient to WeatherService

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class WeatherService {
 
-  constructor() { }
+    forecastUrl = 'assets/data/forecast.json';
+  
+    constructor(private http: HttpClient) { }
+  
+    getConfig() {
+        return this.http.get(this.configUrl);
+    }
 
 }
